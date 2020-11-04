2b2a1350315e9613cf3b11486e63100957b395e7
Jacobcc426 <yc97@vip.qq.com>
2020-11-04 16:27:18 +0800

    I'll learning in home~

---
+++ src/LeetCode9.java
@@ -0,0 +1,16 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class LeetCode9 {
+    public static void main(String[] args) {
+        System.out.println(LeetCode8Solution.myAtoi("-+12"));
+    }
+}
+
+class LeetCode9Solution {
+    public boolean isPalindrome(int x) {
+        String intstr = String.valueOf(x);
+        StringBuffer sb = new StringBuffer("#");
+        return true;
+    }
+}
